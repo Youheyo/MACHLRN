2bdd3830adb07ffd3f975d980f5bb81e7fa88011
Youheyo <80012381+Youheyo@users.noreply.github.com>
2023-06-12 23:25:43 +0800

    idk if it works honestly

--- machlrn2.py
+++ machlrn2.py
@@ -17,17 +17,24 @@ def euclid_dist(point1, point2):
 
 def knn(X_train, y_train, X_Test, k):
 	y_pred = []
-	for test_point in X_Test.values:
+	for test_point in pd.DataFrame(X_Test).values:
 		distances = []
 		for train_point in X_train.values:
 			distance = euclid_dist(test_point, train_point)
 			distances.append(distance)
-		nearest_indices = sorted(range(len(distances)), key=lambda i:distances[i])[k]
+		nearest_indices = sorted(range(len(distances)), key=lambda i:distances[i])[:k]
 		nearest_labels = [y_train.iloc[i] for i in nearest_indices]
 		predicted_label = max(set(nearest_labels), key=nearest_labels.count)
+		# print(predicted_label)
 		y_pred.append(predicted_label)
 	return y_pred
 
-sampleData = [0] * 20
+sampleData = [1] * 20
+# print(sampleData)
 
-print(knn(X, y, sampleData, 3))
+result = (knn(X, y, sampleData, 3))
+# print(result)
+if(result == 0):
+	print("The animal is NOT a pet")
+else:
+	print("The animal is a pet")
