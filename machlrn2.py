f8c3a0dba7f1c938a336a065da1524f067ae469c
Youheyo <80012381+Youheyo@users.noreply.github.com>
2023-06-23 14:06:06 +0800

    Added more prints to track and readability
    It looks like it works could be cause of my dataset resulting in it outputting 0 a lot. Having 0.5 split and 3 K results in at least 1 pet

--- machlrn2.py
+++ machlrn2.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 data = pd.read_csv('animal dataset.csv')\
 
+name = data.iloc[:, 1]
 data = data.iloc[1:, 3:]
 
 X = data.iloc[:, :-1]
@@ -12,9 +13,9 @@ def train_test_split(X, y, test_size=0.2):
 	split_index = int(len(X) * (1 - test_size))
 	X_train, X_test = X[1:split_index], X[split_index:]
 	y_train, y_test = y[1:split_index], y[split_index:]
-	return X_train, X_test, y_train, y_test
+	return X_train, X_test, y_train, y_test, split_index
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, 0.2)
+X_train, X_test, y_train, y_test, split_index = train_test_split(X, y, 0.1)
 
 # print("X Train list", X_train)
 # print("X Test list", X_test)
@@ -26,21 +27,36 @@ def euclid_dist(point1, point2):
 	return dist
 
 def knn(X_train, y_train, X_Test, k):
+	testCount = 0
 	y_pred = []
 	for test_point in pd.DataFrame(X_Test).values:
+		if(testCount > 0): print("- - - - NEXT PET - - - -")
 		distances = []
 		for train_point in X_train.values:
 			distance = euclid_dist(test_point, train_point)
 			distances.append(distance)
 		nearest_indices = sorted(range(len(distances)), key=lambda i:distances[i])[:k]
+		print("Nearest Indice of",name[testCount+split_index-1],":",nearest_indices)
+
 		nearest_labels = [y_train.iloc[i] for i in nearest_indices]
+		print(name[nearest_indices[0]], "is", nearest_labels[0])
+		print(name[nearest_indices[1]], "is", nearest_labels[1])
+		print(name[nearest_indices[2]], "is", nearest_labels[2])
+		#print("Nearest label of current pet",nearest_labels)
+
 		predicted_label = max(set(nearest_labels), key=nearest_labels.count)
 		y_pred.append(predicted_label)
-	print("Distances list ", distances)
-	print("Nearest Indices" , nearest_indices)
+
+		petTag = ""
+		if(y_pred[testCount] == 0): petTag = "not a pet"
+		else: petTag = "is a pet"
+		print(name[testCount+split_index-1], "is predicted to be", petTag)
+		testCount += 1
+
+	print("Number of cases checked:", testCount)
 	return y_pred
 
-y_pred = (knn(X_train, y_train, X_test, 3))
+y_pred = (knn(X_train, y_train, X_test, 5))
 print(y_pred)
 
 def accuracy(y_true, y_pred):
