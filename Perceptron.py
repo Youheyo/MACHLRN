b1b740b865072b10b1a75fef1849172f62532126
Youheyo <80012381+Youheyo@users.noreply.github.com>
2023-07-03 21:47:36 +0800

    Fully Implemented (?). Could use further QoL improvements or even allow User Input but it does work

--- Perceptron.py
+++ Perceptron.py
@@ -2,13 +2,19 @@ import numpy as np
 import pandas as pd
 
 def Split_Data(data, label, test_size=0.2):
+    print("Splitting Data for", test_size * 100 , "% test size")
     split_index = int(len(data) * (1 - test_size))
     X_train, X_test = data[:split_index], data[split_index:]
     y_train, y_test = label[:split_index], label[split_index:]
     return X_train, X_test, y_train, y_test
 
+# TODO: Create a split that randomizes the size
 
+# * Old Way of loading dataset and contains original dataset
+# ! Utilizes Original Dataset where the risk taker tag is undefined and has extra text
+# ! Should still be usable with current code but haven't tested
 """
+
 # Load the dataset
     dataset = pd.read_csv('dataset/kNN Internet Survey Sheet.csv')
 
@@ -30,11 +36,13 @@ def Split_Data(data, label, test_size=0.2):
 
 dataset = pd.read_csv('dataset/kNN Internet Survey Sheet_modified.csv')
 
-#dataset = dataset.iloc[1: :]
+# Split features and labels
 featureset = dataset.iloc[:, 4:-1]
 labelset = dataset.iloc[:, -1]
 
-X_train, X_test, y_train, y_test = Split_Data(featureset, labelset,0.2)
+# * SPLITTING OF DATA
+# Split the dataset for training and testing
+X_train, X_test, y_train, y_test = Split_Data(featureset, labelset, test_size=0.8)
 X_train, X_test, y_train, y_test = X_train.values.astype(float), X_test.values.astype(float), y_train.values.astype(float), y_test.values.astype(float)
 
 
@@ -53,41 +61,57 @@ class Perceptron:
         z = np.dot(self.weights, x) + self.bias
         return self.activation(z)
 
-    def train(self, X, y, learning_rate=0.1, num_epochs=100):
-        testNum = 0
-        for _ in range(num_epochs):
+    def train(self, X, y, learning_rate=0.1, iter=100):
+
+        for _ in range(iter):
             for i, _ in enumerate(X):
                 x = X[i]
                 y_true = y[i]
                 y_pred = self.predict(x)
+                
                 # Update weights and bias based on prediction error
                 self.weights += learning_rate * (y_true - y_pred) * x
                 self.bias += learning_rate * (y_true - y_pred)
-            testNum += 1
-        print("Number of cases trained", testNum)
+            
+        print("TRAINING COMPLETE with..\n", learning_rate * 100, "% learning rate and", iter, "iterations")
 
-# Create a perceptron model
+# Create a perceptron
 perceptron = Perceptron(num_features=X_train.shape[1])
 
-# Train the model
-perceptron.train(X_train, y_train)
+# * Train the perceptron
+perceptron.train(X_train, y_train, learning_rate=0.05,iter=100)
 
-# Test the model
+# Testing
 
 def Accuracy_Test(X_test):
     accuracy = 0
-    testNum = 1
+    tp = fp = tn = fn = 0
+    test_num = 1
     for i, _ in enumerate(X_test):
         x = X_test[i]
         y_true = y_test[i]
         y_pred = perceptron.predict(x)
 
-        if y_pred == y_true:
+        
+        if y_pred == y_true == 1:
+            tp += 1
             accuracy += 1
-        testNum+=1
+        elif y_pred == 1 and y_true == 0:
+            fp += 1
+        elif y_pred == 0 and y_true == 1:
+            fn += 1
+        elif y_pred == y_true == 0:
+            tn += 1
+            accuracy += 1
+        test_num+=1
 
     accuracy /= len(X_test)
-    print("Accuracy test ran for ", testNum)
-    return accuracy
+    
+    print("---------------\nTEST COMPLETE")
+    print("Accuracy:", accuracy)
+    print("True Positive:", tp, "\tFalse Positive:", fp, "\nFalse Negative:", fn,"\tTrue Negative:", tn)
 
-print("Accuracy:", Accuracy_Test(X_test))
+# ? Not sure if needed but it'd be nice especially if have to present
+# TODO Refactor to allow user input
+
+Accuracy_Test(X_test)
